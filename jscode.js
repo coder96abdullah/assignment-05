b11e6e2dc1d6ccc02774bfe04f1c7b68f576f0e6
coder96abdullah <coder96abdullah@gmail.com>
2024-02-19 00:09:48 +0600

    added the remaining work

--- jscode.js
+++ jscode.js
@@ -1,54 +1,95 @@
 // const btns=document.querySelectorAll('.kbd'); 
 let count = 0;
+const arr = [];
 for (const btn of document.querySelectorAll('.kbd')) {
     btn.addEventListener('click', function () {
-        count++;
-        if (count <= 4) {
-            btn.classList.add('bg-green-700', 'text-white');
-            document.getElementById('seat-booked').innerText = count;
-            const num = document.createElement('p');
-            num.innerText = btn.innerText + ' --------------------Economy--------------- ' + '550';
-            document.getElementById('seat-details').appendChild(num);
-            const total=count*550;
-            document.getElementById('total-price').innerText=total.toFixed(2);
-            if(count==4)
-               {
-                document.getElementById('copun').removeAttribute('disabled');
-                document.getElementById('apply-btn').addEventListener('click',function(){
-                    if(document.getElementById('copun').value==='NEW15')
+        if (arr.includes(btn.innerText))
+            alert('Its already taken');
+        else {
 
-                     { 
-    
-                        document.getElementById('vanish').classList.add('hidden');
-                        const tem=document.createElement('p');
-                        const discount=document.getElementById('total-price').innerText*0.15;
-                        tem.innerText='discount : '+discount.toFixed(2);
-                      
-                        document.getElementById('replace').appendChild(tem);
-                        document.getElementById('grand-total').innerText=(total-discount).toFixed(2);
-                     
-                         
-                }  
-                else if(document.getElementById('copun').value==='Couple 20') 
-                {
-                    document.getElementById('vanish').classList.add('hidden');
-                        const tems=document.createElement('p');
-                        const discounts=document.getElementById('total-price').innerText*0.2;
-                        
-                 
-                        document.getElementById('replace').appendChild(tems);
-                        document.getElementById('grand-total').innerText=(total-discounts).toFixed(2);
-                } 
-                else
-                   alert('coupon is not correct'); 
-                })
-                 
-               }
+            arr.push(btn.innerText);
+
+            count++;
+            if (count <= 4) {
+
+                btn.classList.add('bg-green-700', 'text-white');
+                document.getElementById('seat-booked').innerText = count;
+                document.getElementById('seat-leaft').innerText=document.getElementById('seat-leaft').innerText-count;
+                const num = document.createElement('p');
+                num.innerText = btn.innerText + ' --------------------Economy--------------- ' + '550';
+                document.getElementById('seat-details').appendChild(num);
+                const total = count * 550;
+                document.getElementById('total-price').innerText = total.toFixed(2);
+
+                // Next button activate work
+                if (document.getElementById('number-inp').value.length !== 0) {
+                    document.getElementById('next-btn').removeAttribute('disabled');
+                    document.getElementById('next-btn').classList.add('bg-green-700', 'text-white');
+
+        
+                }
+                else {
+                    document.getElementById('number-inp').addEventListener('keyup', function (event) {
+                        if (event.key !== undefined) {
+                            document.getElementById('next-btn').removeAttribute('disabled');
+                            document.getElementById('next-btn').classList.add('bg-green-700', 'text-white');
+                           
+                           
+                        }
+                    })
+
+                }
+
+
+
+
+
+                if (count == 4) {
+                    document.getElementById('copun').removeAttribute('disabled');
+                    document.getElementById('apply-btn').addEventListener('click', function () {
+                        if (document.getElementById('copun').value === 'NEW15') {
+
+                            document.getElementById('vanish').classList.add('hidden');
+                            const tem = document.createElement('p');
+                            const discount = document.getElementById('total-price').innerText * 0.15;
+                            tem.innerText = 'discount : ' + discount.toFixed(2);
+
+                            document.getElementById('replace').appendChild(tem);
+                            document.getElementById('grand-total').innerText = (total - discount).toFixed(2);
+
+
+                        }
+                        else if (document.getElementById('copun').value === 'Couple 20') {
+                            document.getElementById('vanish').classList.add('hidden');
+                            const tems = document.createElement('p');
+                            const discounts = document.getElementById('total-price').innerText * 0.2;
+
+
+                            document.getElementById('replace').appendChild(tems);
+                            document.getElementById('grand-total').innerText = (total - discounts).toFixed(2);
+                        }
+                        else
+                            alert('coupon is not correct');
+                    })
+
+                }
+
+            }
+            else
+                alert('One person can not purchase more than 4 ticket');
+
+
+
+
 
         }
-        else
-            alert('One person can not purchase more than 4 ticket');
-
-
     })
 }
+
+document.getElementById('next-btn').addEventListener('click',function(){
+    document.getElementById('page2').classList.add('hidden');
+    document.getElementById('page1').classList.remove('hidden');
+
+})
+
+
